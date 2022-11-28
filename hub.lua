73a14b0e1dc557cbafcbb88dc7dd841614152218
duccveloper <67620716+duccveloper@users.noreply.github.com>
2022-11-28 17:34:23 +0800

    Create hub.lua

---
+++ hub.lua
@@ -0,0 +1,447 @@
+--[[
+    DuccHub v1.0 by duccveloper
+
+    Why DuccHub v1.0? Where DuccHub v0.1?
+    - Because I accidentally deleted the old version of DuccHub, so I had to remake it. PAIN!
+
+    What is DuccHub?
+    - DuccHub is a hub for all of universal roblox scripts, so you can easily access them all.
+
+    How do I use DuccHub?
+    - Just click execute from your executor, and it will automatically load all of the scripts.
+
+    DuccHub v1.0 changelog:
+    - Recoded all
+    - More neat script
+
+    Credits:
+    - duccveloper - Scripting
+    - deeeity@github - UI Library
+    - Some other dude - Misc scripts
+]]
+local version = '1.0 (Pre-release)'
+
+-- Load UI Library
+local UI_LIB = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+-- Variables
+local images = {
+    ['home_icon'] = 11632424326,
+    ['star_icon'] = 11647714813,
+    ['run_icon'] = 11632434473,
+    ['teleport_icon'] = 11647702726,
+    ['info_icon'] = 11472832266,
+    ['tick_icon'] = 11624388037,
+    ['cross_icon'] = 11624382926,
+    ['warning_icon'] = 11624382860,
+    ['bell_icon'] = 11624378537
+}
+
+-- Services
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+local UIS = game:GetService('UserInputService')
+
+-- Misc variables
+local shifttorun = false
+local walkspeed = LocalPlayer.Character.Humanoid.WalkSpeed
+local runspeed = walkspeed * 2
+local jetpack = false
+local infiniteJump = false
+local target_tp_part = nil
+
+-- Functions
+function checkuserinfo(username)
+    local userid = Players:GetUserIdFromNameAsync(username)
+    local request = 'https://users.roproxy.com/v1/users/' .. userid
+    local response = game:HttpGet(request)
+    local data = game:GetService('HttpService'):JSONDecode(response)
+
+    -- Start fetching data
+    local description = data.description
+    local created = data.created
+    local isbanned = data.isBanned
+    local id = data.id
+    local username = data.name
+    local displayname = data.displayName
+
+    -- Fetch user avatar and headshot url
+    local request =
+        'https://thumbnails.roblox.com/v1/users/avatar?userIds=' .. id .. '&size=150x150&format=Png&isCircular=false'
+    local request2 =
+        'https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=' ..
+        id .. '&size=150x150&format=Png&isCircular=false'
+    local response = game:HttpGet(request)
+    local response2 = game:HttpGet(request2)
+    local data = game:GetService('HttpService'):JSONDecode(response)
+    local data2 = game:GetService('HttpService'):JSONDecode(response2)
+    avatarurl = data.data[1].imageUrl
+    headshoturl = data2.data[1].imageUrl
+end
+
+UIS.InputBegan:Connect(
+    function(input)
+        if input.KeyCode == Enum.KeyCode.LeftShift then
+            if shifttorun then
+                LocalPlayer.Character.Humanoid.WalkSpeed = runspeed
+            end
+        end
+    end
+)
+
+UIS.InputEnded:Connect(
+    function(input)
+        if input.KeyCode == Enum.KeyCode.LeftShift then
+            if shifttorun then
+                LocalPlayer.Character.Humanoid.WalkSpeed = walkspeed
+            end
+        end
+    end
+)
+
+UIS.InputBegan:Connect(
+    function(input)
+        if input.KeyCode == Enum.KeyCode.Space then
+            if jetpack then
+                LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState('Jumping')
+            end
+        end
+    end
+)
+
+UIS.InputBegan:Connect(
+    function(input)
+        if input.KeyCode == Enum.KeyCode.Space and jetpack then
+            while UIS:IsKeyDown(Enum.KeyCode.Space) do
+                wait()
+                LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState('Jumping')
+            end
+        end
+    end
+)
+
+UIS.InputBegan:Connect(
+    function(input)
+        if input.KeyCode == Enum.KeyCode.Space and infiniteJump then
+            LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState('Jumping')
+        end
+    end
+)
+
+function notify(title, text, icon)
+    UI_LIB:Notify(
+        {
+            Title = title,
+            Content = text,
+            Duration = 1.5,
+            Image = icon
+        }
+    )
+end
+
+function GetInstance(str)
+    local pattern = 'Workspace%..'
+	local pattern2 = '^Workspace'
+    if string.match(str, pattern) and string.match(str, pattern2) then
+       return true
+    else
+        
+        notify('Error', 'Pattern invalid!', images['cross_icon'])
+        return false
+    end
+end
+
+
+
+
+function notGetInstance(String)
+	local Table = string.split(String, ".")
+	local Service = game:GetService(Table[1])
+	
+	local ObjectSoFar = Service
+	for Index, Value in pairs(Table) do
+		if Index ~= 1 then
+			local Object = ObjectSoFar:FindFirstChild(Value)
+			if Object then
+				ObjectSoFar = Object
+			else
+				return nil
+			end
+		end
+	end
+	
+	return (ObjectSoFar ~= Service and ObjectSoFar) or nil
+end
+
+
+
+-- UI
+local _UI_Window =
+    UI_LIB:CreateWindow(
+        {
+            Name = 'DuccHub v1.0',
+            LoadingTitle = 'Rayfield Interface Suite',
+            LoadingSubtitle = 'by Sirius',
+            ConfigurationSaving = {
+                Enabled = false,
+                FolderName = nil,
+                FileName = 'Big Hub'
+            },
+            Discord = {
+                Enabled = false,
+                Invite = 'sirius',
+                RememberJoins = true
+            },
+            KeySystem = false,
+            KeySettings = {
+                Title = 'Sirius Hub',
+                Subtitle = 'Key System',
+                Note = 'Join the discord (discord.gg/sirius)',
+                FileName = 'SiriusKey',
+                SaveKey = true,
+                GrabKeyFromSite = false,
+                Key = 'Hello'
+            }
+        }
+    )
+
+-- Tabs
+
+-- Home
+local _UI_T1 = _UI_Window:CreateTab('Home', images['home_icon'])
+local _UI_T1S1 = _UI_T1:CreateSection('Welcome')
+local _UI_T1S1_WelcomeLabel = _UI_T1:CreateLabel('Hi, ' .. LocalPlayer.DisplayName .. '!')
+_UI_T1S1_Leaked = _UI_T1:CreateLabel("quacccc... let's not leak my hard work")
+local _UI_T1S1_TestButton = _UI_T1:CreateButton({
+	Name = "TestLabel",
+	Callback = function()
+		_UI_T1S1_Leaked:Destroy()
+	end,
+})
+local _UI_T1_S2 = _UI_T1:CreateSection('Danger Zone')
+local _UI_T1_S2_DestroyUI =
+    _UI_T1:CreateButton(
+        {
+            Name = 'Destroy UI',
+            Callback = function()
+                UI_LIB:Destroy()
+            end
+        }
+    )
+
+-- Movement
+local _UI_T2 = _UI_Window:CreateTab('Movement', images['run_icon'])
+local _UI_T2_S1 = _UI_T2:CreateSection('Main')
+local _UI_T2_S1_WalkSpeed =
+    _UI_T2:CreateInput(
+        {
+            Name = 'WalkSpeed',
+            PlaceholderText = 'Default: 16',
+            RemoveTextAfterFocusLost = true,
+            Callback = function(Text)
+                if Text == '' then
+                    LocalPlayer.Character.Humanoid.WalkSpeed = 16
+                    notify('WalkSpeed', 'WalkSpeed has been reset to 16', images['tick_icon'])
+                else
+                    tonumber(Text)
+                    if type(Text) == 'number' then
+                        LocalPlayer.Character.Humanoid.WalkSpeed = Text
+                        notify('WalkSpeed', 'WalkSpeed has been set to ' .. Text, images['tick_icon'])
+                    else
+                        notify('Error', 'WalkSpeed must be a number!', images['cross_icon'])
+                    end
+                end
+            end
+        }
+    )
+local _UI_T2_S1_JumpPower =
+    _UI_T2:CreateInput(
+        {
+            Name = 'JumpPower',
+            PlaceholderText = 'Default: 50',
+            RemoveTextAfterFocusLost = true,
+            Callback = function(Text)
+                if Text == '' then
+                    LocalPlayer.Character.Humanoid.JumpPower = 50
+                    notify('JumpPower', 'JumpPower has been reset to 50', images['tick_icon'])
+                else
+                    tonumber(Text)
+                    if type(Text) == 'number' then
+                        LocalPlayer.Character.Humanoid.JumpPower = Text
+                        notify('JumpPower', 'JumpPower has been set to ' .. Text, images['tick_icon'])
+                    else
+                        notify('Error', 'JumpPower must be a number!', images['cross_icon'])
+                    end
+                end
+            end
+        }
+    )
+
+local _UI_T2_S2 = _UI_T2:CreateSection('Shift to Run')
+local _UI_T2_S2_runSpeed =
+    _UI_T2:CreateInput(
+        {
+            Name = 'RunSpeed',
+            PlaceholderText = 'Default: 32',
+            RemoveTextAfterFocusLost = true,
+            Callback = function(Text)
+                -- check if text = number
+                if Text == '' then
+                    runspeed = 32
+                    notify('RunSpeed', 'RunSpeed has been reset to 32', images['tick_icon'])
+                else
+                    tonumber(Text)
+                    if not Text == nil then
+                        runspeed = Text
+                        notify('RunSpeed', 'RunSpeed has been set to ' .. Text, images['tick_icon'])
+                    else
+                        notify('Error', 'RunSpeed must be a number!', images['cross_icon'])
+                    end
+                end
+            end
+        }
+    )
+local _UI_T2_S2_shiftToRun =
+    _UI_T2:CreateToggle(
+        {
+            Name = 'Shift to Run',
+            Callback = function(Value)
+                shifttorun = Value
+            end
+        }
+    )
+
+local _UI_T2_S3 = _UI_T2:CreateSection('Jump')
+local _UI_T2_S3_infiniteJump =
+    _UI_T2:CreateToggle(
+        {
+            Name = 'Infinite Jump',
+            Callback = function(Value)
+                infiniteJump = Value
+            end
+        }
+    )
+local _UI_T2_S3_jetpack =
+    _UI_T2:CreateToggle(
+        {
+            Name = 'Jetpack',
+            Callback = function(Value)
+                jetpack = Value
+            end
+        }
+    )
+
+-- Teleportation
+local _UI_T3 = _UI_Window:CreateTab('Teleportation', images['teleport_icon'])
+local _UI_T3_S1 = _UI_T3:CreateSection('Selection')
+local _UI_T3_S1_selectPart =
+    _UI_T3:CreateButton(
+        {
+            Name = 'Select Part',
+            Callback = function()
+                local tool = Instance.new('Tool')
+                tool.Name = 'Part Selector'
+                tool.RequiresHandle = false
+                tool.CanBeDropped = false
+                tool.Parent = game.Players.LocalPlayer.Backpack
+                notify("Part Selector", "Select a part to teleport to", images['info_icon'])
+                tool.Equipped:Connect(
+                    function(mouse)
+                        mouse.Button1Down:connect(
+                            function()
+                                if mouse.Target and mouse.Target.Parent then
+                                    local part = mouse.Target:GetFullName()
+                                    target_tp_part = part
+                                    _UI_T3_S1_partLabel:Set("Selected part: " .. part)
+                                    target_tp_part_path = GetInstance(target_tp_part)
+                                    tool:Destroy()
+                                end
+                            end
+                        )
+                    end
+                )
+            end
+        }
+    )
+
+-- make user type the path of the part and check if the part is exist or not
+local _UI_T3_S1_partPath =
+    _UI_T3:CreateInput(
+        {
+            Name = 'Part Path',
+            PlaceholderText = 'Must starts with "Workspace"!',
+            RemoveTextAfterFocusLost = true,
+            Callback = function(Text)
+                if Text == '' then
+                    target_tp_part = 'workspace.Part'
+                    _UI_T3_S1_partLabel:Set("No part selected")
+                    target_tp_part_path = nil
+                else
+                    if GetInstance(Text) then
+                       local valididk = notGetInstance(Text)
+                       if valididk == nil then
+                        notify('Error', 'Part does not exist!', images['cross_icon'])
+                          else
+                        target_tp_part = valididk 
+                        notify('Part Path', 'Part Path has been set to ' .. Text, images['tick_icon'])
+                        _UI_T3_S1_partLabel:Set("Selected part: " .. Text)
+                    end
+
+                end
+            end
+        end,
+        }
+    )
+_UI_T3_S1_partLabel = _UI_T3:CreateLabel('No part selected')
+local _UI_T3_S2 = _UI_T3:CreateSection('Teleportation')
+local _UI_T3_S2_teleport =
+    _UI_T3:CreateButton(
+        {
+            Name = 'Teleport',
+            Callback = function()
+                if target_tp_part == nil then
+                    notify('Error', 'No part selected!', images['cross_icon'])
+                else
+                    local part = target_tp_part_path
+                    
+                    LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame + Vector3.new(0, 5, 0)
+                    notify('Teleport', 'Teleported to ' .. target_tp_part, images['tick_icon'])
+                end
+            end,
+        }
+    )
+
+-- Player infoo
+local _UI_T4 = _UI_Window:CreateTab('Player Info', images['info_icon'])
+local _UI_T4_S1 = _UI_T4:CreateSection('Username Input')
+local _UI_T4_S1_username =
+    _UI_T4:CreateInput(
+        {
+            Name = 'Username',
+            PlaceholderText = 'Default: ' .. LocalPlayer.Name,
+            RemoveTextAfterFocusLost = true,
+            Callback = function(Text)
+                if Text == '' then
+                    checkuserinfo(LocalPlayer.Name)
+                else
+                    checkuserinfo(Text)
+                end
+            end
+        }
+    )
+
+-- Player info
+local _UI_T4_S2 = _UI_T4:CreateSection('Player Info')
+local _UI_T4_S2_username = _UI_T4:CreateLabel('Username: ' .. LocalPlayer.Name)
+local _UI_T4_S2_displayname = _UI_T4:CreateLabel('Displayname: ' .. LocalPlayer.DisplayName)
+local _UI_T4_S2_userid = _UI_T4:CreateLabel('UserID: ' .. LocalPlayer.UserId)
+local _UI_T4_S2_copyavatarurl =
+    _UI_T4:CreateButton(
+        {
+            Name = 'Copy Avatar URL',
+            Callback = function()
+                setclipboard(avatarurl)
+                notify('Avatar URL', 'Avatar URL copied to clipboard', images['tick_icon'])
+            end
+        }
+    )

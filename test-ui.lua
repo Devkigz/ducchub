3751171f6dfb9bfb9d26b2ef2b1eda280aaa0c18
duccveloper <67620716+duccveloper@users.noreply.github.com>
2022-11-28 13:58:09 +0000

    fix = fix + 1

--- test-ui.lua
+++ test-ui.lua
@@ -1490,7 +1490,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 				TweenService:Create(Input.InputFrame, TweenInfo.new(0.55, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = UDim2.new(0, Input.InputFrame.InputBox.TextBounds.X + 24, 0, 30)}):Play()
 			end)
 
-			function InputValue:Visible(Value)
+			function InputSettings:Visible(Value)
 				Input.Visible = Value
 			end
 

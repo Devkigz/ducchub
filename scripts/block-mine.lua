ac77434fcd830ab186aa62650649f8ed086af7f7
duccveloper <67620716+duccveloper@users.noreply.github.com>
2023-04-19 18:45:54 +0800

    Create block-mine.lua

---
+++ scripts/block-mine.lua
@@ -0,0 +1,25 @@
+workspace:GetChildren()[181].Return:Destroy()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local Window = Rayfield:CreateWindow({
+   Name = "anti p2w club",
+   LoadingTitle = "imagine kids earning robux by creating shitty sim game",
+   LoadingSubtitle = "by ducc"
+})
+local Tab = Window:CreateTab("main", 4483362458) 
+local Section = Tab:CreateSection("OP down here")
+local Button = Tab:CreateButton({
+   Name = "unlimited strength",
+   Callback = function()
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Anvil"):FireServer(10000000000)
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "free win",
+   Callback = function()
+   firetouchinterest(game:GetService("Players").LocalPlayer.Character.Head, workspace:GetChildren()[181].Touch,1)
+   task.wait()
+      firetouchinterest(workspace:GetChildren()[181].Touch, game:GetService("Players").LocalPlayer.Character.Head, 0)
+
+
+   end,
+})
